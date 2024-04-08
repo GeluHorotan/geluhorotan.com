72655a4fcf3c8e29a6f6e5137386d17bd581be15
GeluHorotan <horotangelu17@gmail.com>
2024-04-08 23:25:31 +0300

    feat: create basic header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import '@app/globals.css';
 
+// Components
+import { Header } from '@components/navigation/Header';
+
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700', '800', '900'],
@@ -18,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <main className="max-w-8xl">
+          <Header />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import Image from 'next/image';
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <div className="max-w-8xl bg-red-400">
-        <h1>TEST testtest dsafs fdsf dsf dsf sasdfas sad asd asdsa</h1>
-      </div>
-    </main>
-  );
+  return <div>TEST</div>;
 }
