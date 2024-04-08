53ad903ed4df0ce3dfe079667fce8105d03d6641
GeluHorotan <horotangelu17@gmail.com>
2024-04-08 23:02:51 +0300

    fix: add foreground and surface colors to body

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-surface text-foreground">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
       <div className="w-1/2">
         <h1>TEST testtest dsafs fdsf dsf dsf sasdfas sad asd asdsa</h1>
       </div>
