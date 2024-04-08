4ffc861c59b9fb684e8b02ae58a2095644946bb3
GeluHorotan <horotangelu17@gmail.com>
2024-04-08 23:02:14 +0300

    feat: create color variables and themes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-surface text-foreground">
       <div className="w-1/2">
         <h1>TEST testtest dsafs fdsf dsf dsf sasdfas sad asd asdsa</h1>
       </div>
