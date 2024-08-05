9be008917871e1a5539b5492bf9946a5f5442d1a
GeluHorotan <horotangelu17@gmail.com>
2024-08-05 21:28:17 +0300

    fix: layout issue in contact form component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,35 +9,30 @@ export function Contact() {
   return (
     <section
       id="contact"
-      className=" w-full  max-w-8xl  flex items-center justify-center px-14  max-[1200px]:px-7 self-center "
+      className=" w-full  max-w-8xl  flex items-center justify-center self-center "
     >
-      <NoiseBackground className="p-12 max-sm:p-6">
-        <div className="p-8 bg-background rounded-xl flex gap-20 max-md:gap-10 max-[1050px]:flex-col-reverse  ">
+      <NoiseBackground noBackground>
+        <div className="  p-8 w-full bg-background rounded-xl flex gap-20 max-[800px]:gap-10 max-md:flex-col-reverse   ">
           <ContactForm />
           <div className="w-1/2 max-[1050px]:w-full  justify-center  flex flex-col gap-8 max-md:gap-4 ">
-            <div className="flex gap-8 flex-col  ">
-              <div className="flex gap-4 flex-col ">
-                <span className="text-[27px] font-semibold">Get in touch!</span>
-                {/* <p>
-                  Let&apos;s build something amazing together! <br /> Drop me a
-                  message or contact me through email and I&apos;ll get back to
-                  you as soon as possible.
-                </p> */}
-              </div>
-            </div>
-            <div className="grid grid-cols-2 max-md:grid-cols-4  w-full gap-4  ">
+            <span className="text-[27px] font-semibold">Get in touch!</span>
+
+            <div className="grid grid-cols-2  w-full gap-4  ">
               {contact_links?.map((link, index) => {
                 return (
                   <div
-                    className={`flex items-center justify-start max-md:justify-center py-4 px-8 max-md:p-3   ${index === 0 || index + 1 === contact_links.length ? 'col-span-2 max-md:col-span-1' : 'max-[1350px]:col-span-2 max-[1050px]:col-span-1  '}   rounded-xl bg-neutral flex  items-center gap-4 overflow-hidden `}
+                    className={`flex rounded-xl bg-neutral  gap-4 items-center justify-start py-4 px-8 max-[520px]:px-4  ${index === 0 || index + 1 === contact_links.length ? 'col-span-2 ' : 'col-span-1 max-[520px]:col-span-2 max-[1050px]:col-span-2 max-md:col-span-1'}    `}
                     key={link?.identifier}
                   >
+                    {/* className=
+                    {`flex items-center justify-start max-md:justify-start py-4 px-8 max-md:p-3   ${index === 0 || index + 1 === contact_links.length ? 'col-span-2 max-md:col-span-1 max-[500px]:col-span-4' : 'max-[1350px]:col-span-2 max-[1050px]:col-span-1 max-[500px]:col-span-4 '}   rounded-xl bg-neutral flex  items-center gap-4 overflow-hidden `}
+                    key={link?.identifier} */}
                     <SvgFinder
                       identifier={link.identifier}
                       size={24}
                       className="fill-foreground "
                     />
-                    <div className="flex flex-col text-[#777] font-medium max-md:hidden ">
+                    <div className="flex flex-col text-[#777] font-medium  ">
                       <span className="text-base  ">{link?.name}</span>
                       <span className="small   ">{link?.id}</span>
                     </div>
--- components/NoiseBackground.tsx
+++ components/NoiseBackground.tsx
@@ -4,18 +4,23 @@ import { cn } from '@lib/utils';
 interface INoiseBackground {
   children: React.ReactNode;
   className?: string;
+  noBackground?: boolean;
 }
 
-export function NoiseBackground({ children, className }: INoiseBackground) {
+export function NoiseBackground({
+  children,
+  className,
+  noBackground,
+}: INoiseBackground) {
   return (
     <div
       className={cn(
-        'w-full relative bg-indigo-800 rounded-xl p-20 [background-image:radial-gradient(88%_100%_at_top,rgba(255,255,255,0.5),rgba(255,255,255,0))]',
+        ` ${noBackground ? 'p-0 background-none rounded-none' : 'bg-indigo-800 rounded-xl p-20 [background-image:radial-gradient(88%_100%_at_top,rgba(255,255,255,0.5),rgba(255,255,255,0))]'} w-full relative `,
         className
       )}
     >
       <NoiseTexture className="rounded-xl" />
-      <div className="z-20 relative rounded-xl shadow-lg">{children}</div>
+      <div className="z-20 relative  shadow-lg">{children}</div>
     </div>
   );
 }
