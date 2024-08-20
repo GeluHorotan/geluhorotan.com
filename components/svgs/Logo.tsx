886f143e606cf502e3fd22c35e1caf4773459c7e
GeluHorotan <horotangelu17@gmail.com>
2024-08-20 18:39:48 +0300

    fix: logo gap issue

--- components/svgs/Logo.tsx
+++ components/svgs/Logo.tsx
@@ -8,27 +8,27 @@ export function Logo({ size = 32, className }: IIconProps) {
       width={size}
       height={size}
       className={className}
-      viewBox="0 0 1019.2 922.03"
+      viewBox="0 0 34.02 31.11"
     >
       <path
         id="Path_1"
         data-name="Path 1"
-        d="M775.92,309.87H480.06V507.03H758.43V685.11H310.47V922.03H1019.2V309.87Z"
-        className="fill-foreground"
+        d="M26.76,12.84H17.93v5.89h8.31v5.31H12.87v7.07H34.02V12.84Z"
+        className="fill-primary"
       />
       <rect
         id="Rectangle_1"
         data-name="Rectangle 1"
-        width="263.94"
-        height="612.15"
-        transform="translate(0 309.87)"
+        width="7.87"
+        height="18.26"
+        transform="translate(0 12.84)"
         className="fill-primary"
       />
       <path
         id="Path_2"
         data-name="Path 2"
-        d="M0,0V262.97H1019.19V0Z"
-        className="fill-foreground"
+        d="M0,0V7.85H34.02V0Z"
+        className="fill-primary"
       />
     </svg>
   );
